b0087d72b62f716bf75f31d56e17d0eea5aaa577
Malin Boyanov <mboyanov@codix.bg>
2017-06-06 18:51:50 +0300

    FormModule moved to Shared

--- src/app/screens/e-fhsel/e-fhsel.module.ts
+++ src/app/screens/e-fhsel/e-fhsel.module.ts
@@ -1,6 +1,5 @@
 import {NgModule} from '@angular/core';
 import {SharedModule} from '../../shared/shared.module';
-import {FormsModule} from '@angular/forms';
 
 import {EFhselRoutingModule} from './e-fhsel-routing.module';
 import {EFhselComponent} from './e-fhsel/e-fhsel.component';
@@ -8,7 +7,6 @@ import {EFhselComponent} from './e-fhsel/e-fhsel.component';
 @NgModule({
   imports: [
     SharedModule,
-    FormsModule,
     EFhselRoutingModule
   ],
   declarations: [EFhselComponent]
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,17 +1,22 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { HttpModule } from '@angular/http';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {HttpModule} from '@angular/http';
+import {FormsModule} from '@angular/forms';
 
-import { AuthService } from './authorization/authorization.services';
-import { AuthorizationComponent } from './authorization/authorization.component';
+import {AuthService} from './authorization/authorization.services';
+import {AuthorizationComponent} from './authorization/authorization.component';
+
 
 @NgModule({
-    providers: [
-        AuthService
-    ],
-    declarations: [AuthorizationComponent],
-    exports: [
-        CommonModule
-    ],
+  providers: [
+    AuthService
+  ],
+  declarations: [AuthorizationComponent],
+  exports: [
+    FormsModule,
+    CommonModule,
+    HttpModule
+  ],
 })
-export class SharedModule { }
+export class SharedModule {
+}
