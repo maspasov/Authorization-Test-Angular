319044553890871d4f584d986b1686387a549645
Martin Spasov <maspasov@codix.bg>
2017-06-08 17:49:47 +0300

    + AuthenticationConnectionBackend

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule  } from '@angular/forms';
-import { Http, HttpModule, RequestOptions } from '@angular/http';
+import { Http, HttpModule, RequestOptions, XHRBackend } from '@angular/http';
 import { CacheLocalStorage, CacheService, CacheStorageAbstract } from 'ng2-cache/ng2-cache';
 
 import { AuthGuard } from './authorization/authorization.guard';
@@ -9,6 +9,7 @@ import { AuthorizationComponent } from './authorization/authorization.component'
 import { AuthService } from './authorization/authorization.services';
 import { DefaultRequestOptions } from './services/default.request.options';
 import { StateHelper } from './services/state.helper';
+import { AuthenticationConnectionBackend } from './services/authenticated-connection.backend';
 
 @NgModule({
   providers: [
@@ -17,7 +18,8 @@ import { StateHelper } from './services/state.helper';
     AuthService,
     { provide: RequestOptions, useClass: DefaultRequestOptions },
     CacheService,
-    { provide: CacheStorageAbstract, useClass: CacheLocalStorage }
+    { provide: CacheStorageAbstract, useClass: CacheLocalStorage },
+    { provide: XHRBackend, useClass: AuthenticationConnectionBackend },
   ],
   imports: [ReactiveFormsModule, CommonModule],
   declarations: [AuthorizationComponent],
